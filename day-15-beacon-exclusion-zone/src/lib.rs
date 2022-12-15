3239e4bf521e6215473a7f2e4f515c5974cc6df8
Miquel van Smoorenburg <mike@langeraar.net>
2022-12-15 17:14:34 +0100

    do range cals ourself

--- day-15-beacon-exclusion-zone/src/lib.rs
+++ day-15-beacon-exclusion-zone/src/lib.rs
@@ -1,5 +1,3 @@
-use std::ops::RangeInclusive;
-use btree_range_map::RangeSet;
 use itertools::Itertools;
 use runner::*;
 
@@ -8,8 +6,12 @@ pub fn start(ctx: &mut Ctx) {
 
     let grid = Grid::parse(input);
     ctx.update_timer(Ctx::PARSING);
+    /*
+    for s in &grid.sensors {
+        println!("{:?}", s);
+    }*/
 
-    outputln!(ctx, "part1: {}", grid.no_beacons_at_y(2000000));
+    outputln!(ctx, "part1: {}", grid.nr_beacons_at_y(2000000));
     ctx.update_timer(Ctx::PART1);
 }
 
@@ -41,13 +43,13 @@ impl Sensor {
         }
     }
 
-    fn range_on_y(&self, y: i32) -> Option<RangeInclusive<i32>> {
+    fn range_on_y(&self, y: i32) -> Option<(i32, i32)> {
         let d = self.sy.abs_diff(y) as i32;
         if d > self.area {
             return None;
         }
         let w = self.area - d;
-        Some(self.sx - w ..= self.sx + w)
+        Some((self.sx - w, self.sx + w))
     }
 }
 
@@ -65,14 +67,67 @@ impl Grid {
         Grid { sensors }
     }
 
-    fn no_beacons_at_y(&self, y: i32) -> u32 {
-        let mut set = RangeSet::new();
+    fn nr_beacons_at_y(&self, y: i32) -> i32 {
+        let mut set = Ranges::default();
         for r in self.sensors.iter().filter_map(|s| s.range_on_y(y)) {
-            set.insert(r);
+            set.insert(r.0, r.1);
         }
-        for r in self.sensors.iter().filter(|s| s.by == y).map(|s| (s.bx ..= s.bx)) {
+        for r in self.sensors.iter().filter(|s| s.by == y).map(|s| s.bx) {
             set.remove(r);
         }
+        set.merge();
         set.len()
+    }
+}
+
+#[derive(Default)]
+struct Ranges {
+    r: Vec<(i32, i32)>
+}
+
+impl Ranges {
+    fn insert(&mut self, r1: i32, r2: i32) {
+        self.r.push((r1, r2));
+    }
+
+    fn remove(&mut self, x: i32) {
+        let len = self.r.len();
+        for idx in 0 .. len {
+            if self.r[idx].0 <= x && x<= self.r[idx].1 {
+                let e = self.r[idx].1;
+                self.r[idx].1 = x - 1;
+                self.r.push((x + 1, e));
+            }
+        }
+    }
+
+    fn merge(&mut self) {
+        self.r.sort_unstable_by(|a, b| a.0.cmp(&b.0));
+        self.r.retain(|(x, y)| x <= y);
+        if self.r.len() <= 1 {
+            return;
+        }
+        let mut n1 = 0;
+        let mut n2 = 1;
+        while n2 < self.r.len() {
+            if self.r[n1].1 >= self.r[n2].1 {
+                // skip
+                n2 += 1;
+            } else if self.r[n1].1 >= self.r[n2].0 {
+                // merge
+                self.r[n1].1 = self.r[n2].1;
+                n2 += 1;
+            } else {
+                // advance
+                self.r[n1 + 1] = self.r[n2];
+                n1 += 1;
+                n2 += 1;
+            }
+        }
+        self.r.truncate(n1 + 1);
+    }
+
+    fn len(&self) -> i32 {
+        self.r.iter().fold(0i32, |acc, x| acc + (x.1 - x.0) + 1)
     }
 }
