b16aaa4f73ea0186fac2eb6a93b04450eec6fc21
Miquel van Smoorenburg <mike@langeraar.net>
2022-12-15 17:48:35 +0100

    day 15 part2 -- brute force

--- day-15-beacon-exclusion-zone/src/lib.rs
+++ day-15-beacon-exclusion-zone/src/lib.rs
@@ -6,13 +6,12 @@ pub fn start(ctx: &mut Ctx) {
 
     let grid = Grid::parse(input);
     ctx.update_timer(Ctx::PARSING);
-    /*
-    for s in &grid.sensors {
-        println!("{:?}", s);
-    }*/
 
-    outputln!(ctx, "part1: {}", grid.nr_beacons_at_y(2000000));
+    outputln!(ctx, "part1: {}", grid.no_beacons_at_y(2000000));
     ctx.update_timer(Ctx::PART1);
+
+    outputln!(ctx, "part2: {}", grid.find_gap_brute_force(4000000));
+    ctx.update_timer(Ctx::PART2);
 }
 
 #[derive(Debug)]
@@ -67,20 +66,39 @@ impl Grid {
         Grid { sensors }
     }
 
-    fn nr_beacons_at_y(&self, y: i32) -> i32 {
+    fn get_raw_ranges_at_y(&self, y: i32, rm_beacons: bool) -> Ranges {
         let mut set = Ranges::default();
         for r in self.sensors.iter().filter_map(|s| s.range_on_y(y)) {
             set.insert(r.0, r.1);
         }
-        for r in self.sensors.iter().filter(|s| s.by == y).map(|s| s.bx) {
-            set.remove(r);
+        if rm_beacons {
+            for r in self.sensors.iter().filter(|s| s.by == y).map(|s| s.bx) {
+                set.remove(r);
+            }
         }
-        set.merge();
-        set.len()
+        set
+    }
+
+    fn no_beacons_at_y(&self, y: i32) -> i32 {
+        let mut r = self.get_raw_ranges_at_y(y, true);
+        r.merge();
+        r.len()
+    }
+
+    fn find_gap_brute_force(&self, limit: i32) -> u64 {
+        for y in 0 ..= limit {
+            let mut r = self.get_raw_ranges_at_y(y, false);
+            r.clamp(0, limit);
+            r.merge();
+            if r.num_ranges() != 1 {
+                return (r.r[0].1 + 1) as u64 * 4000000 + y as u64;
+            }
+        }
+        panic!("find_gap: FAIL");
     }
 }
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 struct Ranges {
     r: Vec<(i32, i32)>
 }
@@ -97,6 +115,23 @@ impl Ranges {
                 let e = self.r[idx].1;
                 self.r[idx].1 = x - 1;
                 self.r.push((x + 1, e));
+            }
+        }
+    }
+
+    fn clamp(&mut self, low: i32, high: i32) {
+        for r in &mut self.r {
+            if r.0 < low {
+                r.0 = low;
+            }
+            if r.0 > high {
+                r.1 = r.0 - 1;
+            }
+            if r.1 < low {
+                r.1 = r.0 - 1;
+            }
+            if r.1 > high {
+                r.1 = high;
             }
         }
     }
@@ -129,5 +164,9 @@ impl Ranges {
 
     fn len(&self) -> i32 {
         self.r.iter().fold(0i32, |acc, x| acc + (x.1 - x.0) + 1)
+    }
+
+    fn num_ranges(&self) -> i32 {
+        self.r.len() as i32
     }
 }
