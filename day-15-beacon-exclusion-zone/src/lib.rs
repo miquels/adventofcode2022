1ba5ec4f9e846cc3ccdcf3cc7a4af93b481c6721
Miquel van Smoorenburg <mike@langeraar.net>
2022-12-15 18:15:12 +0100

    tidy up

--- day-15-beacon-exclusion-zone/src/lib.rs
+++ day-15-beacon-exclusion-zone/src/lib.rs
@@ -69,7 +69,7 @@ impl Grid {
     fn get_raw_ranges_at_y(&self, y: i32, rm_beacons: bool) -> Ranges {
         let mut set = Ranges::default();
         for r in self.sensors.iter().filter_map(|s| s.range_on_y(y)) {
-            set.insert(r.0, r.1);
+            set.insert(Range{ start: r.0, end: r.1, x: 0 });
         }
         if rm_beacons {
             for r in self.sensors.iter().filter(|s| s.by == y).map(|s| s.bx) {
@@ -91,66 +91,84 @@ impl Grid {
             r.clamp(0, limit);
             r.merge();
             if r.num_ranges() != 1 {
-                return (r.r[0].1 + 1) as u64 * 4000000 + y as u64;
+                return (r.r[0].end + 1) as u64 * 4000000 + y as u64;
             }
         }
         panic!("find_gap: FAIL");
     }
 }
 
+// an inclusive from ..= to range, with a extra payload 'x'.
+#[derive(Debug, Clone, Copy)]
+struct Range {
+    start: i32,
+    end: i32,
+    x: i32,
+}
+
+// A set of inclusive ranges.
 #[derive(Default, Debug)]
 struct Ranges {
-    r: Vec<(i32, i32)>
+    r: Vec<Range>,
 }
 
 impl Ranges {
-    fn insert(&mut self, r1: i32, r2: i32) {
-        self.r.push((r1, r2));
+    // Insert a range.
+    fn insert(&mut self, range: Range) {
+        self.r.push(range);
     }
 
+    // Remove a range of length 1.
     fn remove(&mut self, x: i32) {
         let len = self.r.len();
         for idx in 0 .. len {
-            if self.r[idx].0 <= x && x<= self.r[idx].1 {
-                let e = self.r[idx].1;
-                self.r[idx].1 = x - 1;
-                self.r.push((x + 1, e));
+            if self.r[idx].start <= x && x<= self.r[idx].end {
+                let mut r2 = self.r[idx];
+                self.r[idx].end = x - 1;
+                r2.start = x + 1;
+                self.r.push(r2);
             }
         }
     }
 
+    // Clamp ranges. If a range is outside of low ..= high, it will
+    // be made a range of length 0 by setting r.start to r.end + 1.
+    // We'll filter those out in merge().
     fn clamp(&mut self, low: i32, high: i32) {
         for r in &mut self.r {
-            if r.0 < low {
-                r.0 = low;
+            if r.start < low {
+                r.start = low;
             }
-            if r.0 > high {
-                r.1 = r.0 - 1;
+            if r.start > high {
+                r.end = r.start - 1;
             }
-            if r.1 < low {
-                r.1 = r.0 - 1;
+            if r.end < low {
+                r.end = r.start - 1;
             }
-            if r.1 > high {
-                r.1 = high;
+            if r.end > high {
+                r.end = high;
             }
         }
     }
 
+    // Remove empty ranges, then sort.
+    fn clean(&mut self) {
+        self.r.sort_unstable_by(|a, b| a.start.cmp(&b.start));
+        self.r.retain(|r| r.start <= r.end);
+    }
+
+    // Merge ranges, remove ranges of length 0.
     fn merge(&mut self) {
-        self.r.sort_unstable_by(|a, b| a.0.cmp(&b.0));
-        self.r.retain(|(x, y)| x <= y);
-        if self.r.len() <= 1 {
-            return;
-        }
+        self.clean();
         let mut n1 = 0;
         let mut n2 = 1;
         while n2 < self.r.len() {
-            if self.r[n1].1 >= self.r[n2].1 {
+            if self.r[n1].end >= self.r[n2].end {
                 // skip
                 n2 += 1;
-            } else if self.r[n1].1 >= self.r[n2].0 {
+            } else if self.r[n1].end >= self.r[n2].start {
                 // merge
-                self.r[n1].1 = self.r[n2].1;
+                self.r[n1].end = self.r[n2].end;
                 n2 += 1;
             } else {
                 // advance
@@ -162,10 +180,14 @@ impl Ranges {
         self.r.truncate(n1 + 1);
     }
 
+    // Sum of length of all ranges.
+    // Only useful after merge().
     fn len(&self) -> i32 {
-        self.r.iter().fold(0i32, |acc, x| acc + (x.1 - x.0) + 1)
+        self.r.iter().fold(0i32, |acc, x| acc + (x.end - x.start) + 1)
     }
 
+    // Number of ranges.
+    // Only useful after merge().
     fn num_ranges(&self) -> i32 {
         self.r.len() as i32
     }
